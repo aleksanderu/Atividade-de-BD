7ed4d877d5b088b409df032c1aa7819385bf3ef0
aleksanderu <91164524+aleksanderu@users.noreply.github.com>
2022-03-20 16:20:11 -0300

    primeiro script SQL

---
+++ jogos.sql
@@ -0,0 +1,41 @@
+create database jogos;
+
+create table rpg(
+nome varchar (100) not null,
+ano int (4) not null);
+
+create table luta(
+nome varchar (100) not null,
+ano int (4) not null);
+
+create table aventura(
+nome varchar (100) not null,
+ano int (4) not null);
+
+create table beat'em up(
+nome varchar (100) not null,
+ano int (4) not null);
+
+create table simulador(
+nome varchar (100) not null,
+ano int (4) not null);
+
+create table recreacao(
+nome varchar (100) not null,
+ano int (4) not null);
+
+create table soul(
+nome varchar (100) not null,
+ano int (4) not null);
+
+create table sports(
+nome varchar (100) not null,
+ano int (4) not null;
+
+create table terror(
+nome varchar (100) not null,
+ano int (4) not null;
+
+create table FPS(
+nome varchar (100) not null,
+ano int (4) not null;
